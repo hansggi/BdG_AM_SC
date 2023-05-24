Fixed sign error + general improvements

analysis.py
@@ -1,73 +1,84 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib as mpl
 
 
-"""Tcs = np.load(r"C:\Users\hansggi\OneDrive - NTNU\BdG\Tcs.npy", allow_pickle=True)
-mgs = np.linspace(0, 0.3, 20)
-plt.plot(mgs, Tcs, label = "straight")
-TcsSkew = np.load(r"C:\Users\hansggi\OneDrive - NTNU\BdG\TcsSkewed.npy", allow_pickle=True)
-plt.plot(mgs, TcsSkew, label = "skewed")
-plt.legend()
-plt.show()
+# -- The single material, not heterostructure part --
+"""mzs, NDelta, Ny, result = np.load("C:/Users/hansggi/OneDrive - NTNU/BdG/NewData/MzOneMatND=10Ny=10.npy", allow_pickle=True)
 
-# mgs = np.linspace(0, 2, 20)
-# mgs, TcsP = np.load("Parallell_mgdata.npy")
-# print(mgs)
+mgs, NDelta2, Ny2, result2 = np.load("C:/Users/hansggi/OneDrive - NTNU/BdG/NewData/MgOneMatND=10Ny=10.npy", allow_pickle=True)
 
-# mgs, TcsAP = np.load("AParallell_mgdata.npy")
-# print(mgs)
-# TcTrue = 0.0971954345703125 # 500 NDelta, 20x20, mg = 0.2
-# plt.plot(mgs, TcsP, label = "P")
-# plt.plot(mgs, TcsAP, label = "AP")
-# plt.legend()
-# plt.show()
+font = {'size'   : 14}
+
+mpl.rc('font', **font)
+
+fig, ax = plt.subplots()
+
+ax.plot(mzs, result, label = "FM")
+ax.set_xlabel("Magnetic strength")
+ax.set_ylabel(r"Critical temperature $T_c$")
+ax.plot(mgs, result2, label = "AM")
+ax.legend(loc = "best")
 
 
-# mgs, TcsTestP = np.load("testdata\P_ND=15,Ny=5.npy")
-# mgs, TcsTestP2 = np.load("testdata\P_ND=100,Ny=5.npy")
-# mgs, TcsTestP3 = np.load("testdata\P_ND=200,Ny=5.npy", label = "Par")
+plt.tight_layout()
 
-# plt.plot(mgs, TcsTestP)
-# plt.plot(mgs, TcsTestP2)
-# plt.plot(mgs, TcsTestP, label = "P, ND=15")
-
-# mgs, TcsTestAP  = np.load("testdata\AP_ND=15,Ny=5.npy")
-# mgs, TcsTestAP2 = np.load("testdata\AP_ND=100,Ny=5.npy")
-# mgs, TcsTestAP3 = np.load("testdata\AP_ND=200,Ny=5.npy")
-
-# plt.plot(mgs, TcsTestAP, linestyle = "dashed", label = "AP, ND=15" )
-# plt.plot(mgs, TcsTestAP2)
-# plt.plot(mgs, TcsTestAP, label = "AP")
-
-
-mgs, TcsTestP  = np.load(r"C:\Users\hansggi\OneDrive - NTNU\BdG\testdata\Max_P_ND=15,Ny=5.npy")
-mgs, TcsTestAP  = np.load(r"C:\Users\hansggi\OneDrive - NTNU\BdG\testdata\Max_AP_ND=15,Ny=5.npy")
-
-# mgs, TcsTestAP2 = np.load("testdata\AP_ND=100,Ny=5.npy")
-# mgs, TcsTest = np.load("testdata\AP_ND=200,Ny=5.npy")
-
-plt.plot(mgs, TcsTestP, label = "P_max")
-plt.plot(mgs, TcsTestAP, label = "AP_max")
-
-# plt.xlabel("Altermagnet strength m")
-# plt.ylabel("Tc, averaging over SC")
-
-# plt.legend()
-# plt.show()
-
-mgs, TcsTestP  = np.load(r"C:\Users\hansggi\OneDrive - NTNU\BdG\testdata\Max_P_ND=15,Ny=20.npy")
-mgs, TcsTestAP  = np.load(r"C:\Users\hansggi\OneDrive - NTNU\BdG\testdata\Max_AP_ND=15,Ny=20.npy")
-
-# mgs, TcsTestAP2 = np.load("testdata\AP_ND=100,Ny=5.npy")
-# mgs, TcsTest = np.load("testdata\AP_ND=200,Ny=5.npy")
-
-# plt.plot(mgs, TcsTestP, label = "P_average")
-# plt.plot(mgs, TcsTestAP, label = "AP_average")
-
-plt.xlabel("Altermagnet strength m")
-plt.ylabel("Tc, averaging over SC")
-
-plt.legend()
+fig.savefig("C:/Users/hansggi/OneDrive - NTNU/BdG/figsAMSC/onemat.pdf", format = "pdf", bbox_inches="tight")
 plt.show()"""
+# -- End --------------------------------------
 
-mgs, NDelta, Ny, result = np.load("NewData\OneMatND=1Ny)=10mgs10=[(0.0, 0.1)].npy")
+# -- Junction geometry part --
+"""mgsStraight, NDelta, Ny, resultStraight = np.load("C:/Users/hansggi/OneDrive - NTNU/BdG/NewData/STRAIGHT=30Ny=10.npy", allow_pickle=True)
+
+mgsSkewed, NDelta2, Ny2, resultSkewed = np.load("C:/Users/hansggi/OneDrive - NTNU/BdG/NewData/SKEWED=30Ny=10.npy", allow_pickle=True)
+
+font = {'size'   : 14}
+
+mpl.rc('font', **font)
+
+fig, ax = plt.subplots()
+print(resultStraight[0], resultSkewed[0])
+resultStraight = resultStraight / resultStraight[0]
+resultSkewed = resultSkewed / resultSkewed[0]
+ax.plot(mgsStraight, resultStraight, label = "Straight")
+ax.set_ylim(np.amin(resultStraight)*0.99, np.amax(resultStraight)*1.01)
+
+ax.plot(mgsSkewed, resultSkewed, label = "Skewed")
+ax.set_xlabel("Altermagnetic strength m")
+ax.set_ylabel(r"Critical temperature $T_c(m)/T_c(m=0)$")
+ax.legend(loc = "best")
+
+
+plt.tight_layout()
+
+fig.savefig("C:/Users/hansggi/OneDrive - NTNU/BdG/figsAMSC/geometry.pdf", format = "pdf", bbox_inches="tight")
+plt.show()"""
+# -- End --------------------------
+
+# -- P/AP part --
+"""mgsP, NDelta, Ny, resultP = np.load("C:/Users/hansggi/OneDrive - NTNU/BdG/NewData/P=30Ny=10.npy", allow_pickle=True)
+
+mgsAP, NDelta2, Ny2, resultAP = np.load("C:/Users/hansggi/OneDrive - NTNU/BdG/NewData/AP=30Ny=10.npy", allow_pickle=True)
+
+font = {'size'   : 14}
+
+mpl.rc('font', **font)
+
+fig, ax = plt.subplots()
+print(resultP[0], resultAP[0])
+resultP = resultP / resultP[0]
+resultAP = resultAP / resultAP[0]
+ax.plot(mgsP, resultP, label = "P")
+ax.set_ylim(np.amin(resultP)*0.99, np.amax(resultP)*1.01)
+
+ax.plot(mgsAP, resultAP, label = "Skewed")
+ax.set_xlabel("Altermagnetic strength m")
+ax.set_ylabel(r"Critical temperature $T_c(m)/T_c(m=0)$")
+ax.legend(loc = "best")
+
+
+plt.tight_layout()
+
+fig.savefig("C:/Users/hansggi/OneDrive - NTNU/BdG/figsAMSC/PAP.pdf", format = "pdf", bbox_inches="tight")
+plt.show()"""
+# -- End -----------------------

funcs.py
@@ -121,43 +121,28 @@ def make_H_numba(Nx, Ny, m_arr, mz_arr, Delta_arr, mu, skewed):
                     
 # Skewed ---------------------------------------------------------------------------
             elif skewed: 
-                # m = mg
-                # print("THIS IS NOT VERIFIED!")
-                # if ix < bd[0]:
-                #     m = m_arr[iy, ix]
-                #     mz = mz_arr[iy, ix]
-                # else:
-                #     m = 0
-                #     mz = 0
                 # Here, have used a system where the first row is further to the left than the second row etc,
-                # also, the boundary splits each row in two. 
-                # x-hopping------------------------ --
-                assert Ny % 2 ==0
+                # also, the boundary splits each row in two. m_arr is NyxNx matrix, where indexing is as if
+                # the grid was square.
                 # On even site in y- direction ----------------------------------------------------------
+                assert Ny % 2 ==0 # will get errors if not, due to periodic bdc in y-direction
                 if iy % 2 == 0:
-
                     # -- From the right --
-                    # if ix == bd[0] -1 : # Just hopping, not atermagnetism, depends on both sites
-                    # H[i, (i - Nx ) % (Nx*Ny) ] =nb_block(((t * I2,  0 * I2),
-                    #                                     (0 * I2, -t * I2)))
-                    # else:
-                    m = min(m_arr[iy, ix], m_arr[iy - 1, ix + 1])
-                    H[i, (i - Nx)% (Nx*Ny)  ] += nb_block(((t * I2 + m*sigmaz , 0 * I2),
+                    m = min(m_arr[iy, ix], m_arr[(iy - 1)%Ny, ix ])
+                    H[i, (i - Nx)% (Nx*Ny)  ] = nb_block(((t * I2 + m*sigmaz , 0 * I2),
                                                           (0 * I2, -t * I2 - m*sigmaz)))
                         
                     # -- From the left --
                     if ix > 0:
-                        #if ix == bd[0]: m already zero
-                        m = min(m_arr[iy, ix], m_arr[iy + 1, ix - 1])
-                        H[i, (i + Nx - 1)% (Nx*Ny) ] += nb_block(((t * I2 + m*sigmaz , 0 * I2),
+                        m = min(m_arr[iy, ix], m_arr[(iy + 1)%Ny, ix - 1])
+                        H[i, (i + Nx - 1)% (Nx*Ny) ] = nb_block(((t * I2 + m*sigmaz , 0 * I2),
                                                                  (0 * I2, -t * I2 - m*sigmaz)))
 
-                    # -- y hopping --
-
+                    # -- y hopping, opposite sign for the altermangetic terms --
                     # -- From above --
                     if ix > 0:
-                        m = min(m_arr[iy, ix], m_arr[iy - 1, ix - 1])
-                        H[i,( i - Nx -1)% (Nx*Ny) ] += nb_block(((t * I2 - m*sigmaz , 0 * I2),
+                        m = min(m_arr[iy, ix], m_arr[(iy - 1)%Ny, ix - 1])
+                        H[i,( i - Nx -1)% (Nx*Ny) ] = nb_block(((t * I2 - m*sigmaz , 0 * I2),
                                                                 (0 * I2, -t * I2 + m*sigmaz)))
                             
                     # -- From below --
@@ -165,8 +150,8 @@ def make_H_numba(Nx, Ny, m_arr, mz_arr, Delta_arr, mu, skewed):
                     #     H[i, (i + Nx ) % (Nx*Ny)] = nb_block(((t * I2  , 0 * I2),
                     #                                           (0 * I2, -t * I2 )))
                     # else:
-                    m = min(m_arr[iy, ix], m_arr[iy + 1, ix])
-                    H[i, (i + Nx ) % (Nx*Ny)] += nb_block(((t * I2 - m*sigmaz , 0 * I2),
+                    m = min(m_arr[iy, ix], m_arr[(iy + 1)%Ny, ix])
+                    H[i, (i + Nx ) % (Nx*Ny)] = nb_block(((t * I2 - m*sigmaz , 0 * I2),
                                                         (0 * I2, -t * I2 + m*sigmaz)))
                 # ---------------------------------------------------------------------------
                 # On odd site in y-direction ------------------------------------------------
@@ -174,9 +159,9 @@ def make_H_numba(Nx, Ny, m_arr, mz_arr, Delta_arr, mu, skewed):
 
                     # From the right
                     if  ix  < Nx - 1:
-                        m = min(m_arr[iy, ix], m_arr[iy - 1, ix + 1])
-                        H[i, (i - Nx +1)% (Nx*Ny) ] += nb_block(((t * I2 + m*sigmaz , 0 * I2),
-                                                         (0 * I2, -t * I2 - m*sigmaz)))
+                        m = min(m_arr[iy, ix], m_arr[(iy - 1)%Ny, ix + 1])
+                        H[i, (i - Nx +1)% (Nx*Ny) ] = nb_block(((t * I2 + m*sigmaz , 0 * I2),
+                                                                 (0 * I2, -t * I2 - m*sigmaz)))
                         # if ix == bd[0] -1:
                         #     H[i, (i - Nx +1) % (Nx*Ny)] += nb_block(((t * I2 , 0 * I2),
                         #                                         (0 * I2, -t * I2 )))
@@ -185,22 +170,22 @@ def make_H_numba(Nx, Ny, m_arr, mz_arr, Delta_arr, mu, skewed):
                         #                                  (0 * I2, -t * I2 - m*sigmaz)))
                         
                     # -- From the left --
-                    m = min(m_arr[iy, ix], m_arr[iy + 1, ix])
-                    H[i, (i + Nx) % (Nx*Ny)  ] += nb_block(((t * I2 + m*sigmaz , 0 * I2),
+                    m = min(m_arr[iy, ix], m_arr[(iy + 1)%Ny, ix])
+                    H[i, (i + Nx) % (Nx*Ny)  ] = nb_block(((t * I2 + m*sigmaz , 0 * I2),
                                                             (0 * I2, -t * I2 - m*sigmaz)))
 
                     # -- y hopping --
 
                     # -- From above --
-                    m = min(m_arr[iy, ix], m_arr[iy - 1, ix])
-                    H[i,( i - Nx % (Nx*Ny)) ] += nb_block(((t * I2 - m*sigmaz , 0 * I2),
-                                                (0 * I2, -t * I2 + m*sigmaz)))
+                    m = min(m_arr[iy, ix], m_arr[(iy - 1)%Ny, ix])
+                    H[i,( i - Nx % (Nx*Ny)) ] = nb_block(((t * I2 - m*sigmaz , 0 * I2),
+                                                           (0 * I2, -t * I2 + m*sigmaz)))
                 
                     # -- From below --
                     if  ix < Nx - 1:
-                        m = min(m_arr[iy, ix], m_arr[iy + 1, ix +1])
-                        H[i, (i + Nx + 1)% (Nx*Ny)] += nb_block(((t * I2 - m*sigmaz , 0 * I2),
-                                                                (0 * I2, -t * I2 + m*sigmaz)))
+                        m = min(m_arr[iy, ix], m_arr[(iy + 1)%Ny, ix +1])
+                        H[i, (i + Nx + 1)% (Nx*Ny)] = nb_block(((t * I2 - m*sigmaz , 0 * I2),
+                                                                 (0 * I2, -t * I2 + m*sigmaz)))
                         
                         # if ix == bd[0] -1:
                         #     H[i, (i + Nx + 1)% (Nx*Ny)] += nb_block(((t * I2  , 0 * I2),
@@ -214,7 +199,7 @@ def make_H_numba(Nx, Ny, m_arr, mz_arr, Delta_arr, mu, skewed):
 
     # assert np.allclose(Hr, np.transpose(np.conjugate(Hr)))
     # print(np.allclose(Hr, np.transpose(np.conjugate(Hr))))
-    return Hr
+    return - Hr
 
 """@njit
 def make_H_FT(Nx, Ny, m_arr0, mz_arr0, Delta_arr0, U, mu, bd, skewed):
@@ -270,6 +255,7 @@ def Delta_sc(gamma, D, Ui, T):
     u_dn = gamma[1::4, :]
     v_up = gamma[2::4, :]
     v_dn = gamma[3::4, :]
+    # print(D[-1])
     # Make Ui
     # Ui = np.ones(Nx*Ny, dtype = complex).reshape(Ny, Nx)*U
 
@@ -278,7 +264,7 @@ def Delta_sc(gamma, D, Ui, T):
     f  = (1 - np.tanh(  D / ( 2 * T ))) / 2
     # fm = (1 - np.tanh( -D / ( 2 * T ))) / 2
     # -------------------------------------------------------------------
-    Delta_i = -Ui*np.sum(u_dn *np.conjugate(v_up) * f +       u_up * np.conjugate(v_dn)* (1 - f) , axis = 1) # Here, we used equilibrium explicitly to say (1-f(E)) = f(-E) 
+    Delta_i = Ui*np.sum(u_dn *np.conjugate(v_up) * f +       u_up * np.conjugate(v_dn)* (1 - f) , axis = 1) # Here, we used equilibrium explicitly to say (1-f(E)) = f(-E) 
     return Delta_i
 
 
@@ -298,12 +284,12 @@ def F_sc(gamma, D, V, T, Nx, Ny, bd):
             i = ix + Nx *iy
 
             if ix < Nx-1:
-                F[i, i + 1] = Vi[i]* np.sum(f(D, T) * ( np.conjugate(v_up[i])*u_dn[i+1] - u_up[i]* np.conjugate(v_dn[i + 1]) ) +   u_up[i]* np.conjugate(v_dn[i + 1]), axis = 1)
-                F[i + 1, i] = Vi[i]* np.sum(f(D, T) * ( np.conjugate(v_up[i+1])*u_dn[i] - u_up[i + 1]* np.conjugate(v_dn[i]) ) +   u_up[i + 1]* np.conjugate(v_dn[i]), axis = 1)
+                F[i, i + 1] = Vi[i]* np.sum(fd(D, T) * ( np.conjugate(v_up[i])*u_dn[i+1] - u_up[i]* np.conjugate(v_dn[i + 1]) ) +   u_up[i]* np.conjugate(v_dn[i + 1]), axis = 1)
+                F[i + 1, i] = Vi[i]* np.sum(fd(D, T) * ( np.conjugate(v_up[i+1])*u_dn[i] - u_up[i + 1]* np.conjugate(v_dn[i]) ) +   u_up[i + 1]* np.conjugate(v_dn[i]), axis = 1)
 
             if ix > 0:
-                F[i, i - 1] = Vi[i]* np.sum(f(D, T) * ( np.conjugate(v_up[i]) * u_dn[i - 1] - u_up[i] * np.conjugate(v_dn[i - 1]) ) +   u_up[i]* np.conjugate(v_dn[i - 1]), axis = 1)
-                F[i - 1, i] = Vi[i]* np.sum(f(D, T) * ( np.conjugate(v_up[i - 1]) * u_dn[i] - u_up[i - 1] * np.conjugate(v_dn[i]) ) +   u_up[i - 1]* np.conjugate(v_dn[i]), axis = 1)
+                F[i, i - 1] = Vi[i]* np.sum(fd(D, T) * ( np.conjugate(v_up[i]) * u_dn[i - 1] - u_up[i] * np.conjugate(v_dn[i - 1]) ) +   u_up[i]* np.conjugate(v_dn[i - 1]), axis = 1)
+                F[i - 1, i] = Vi[i]* np.sum(fd(D, T) * ( np.conjugate(v_up[i - 1]) * u_dn[i] - u_up[i - 1] * np.conjugate(v_dn[i]) ) +   u_up[i - 1]* np.conjugate(v_dn[i]), axis = 1)
 
             # if iy > 0:
             F[i, (i + Nx)%(Nx*Ny)] = Vi[i]* np.sum(f(D, T) * ( np.conjugate(v_up[i])*u_dn[(i + Nx) % (Nx*Ny)] - u_up[i]* np.conjugate(v_dn[(i + Nx) % (Nx*Ny)]) ) +   u_up[i]* np.conjugate(v_dn[(i+Nx)%(Nx*Ny)]), axis = 1)

simple_diag.py
@@ -18,7 +18,6 @@ from icecream import ic
 # @njit()
 def does_Delta_increase(Nx, Ny,m_arr, mz_arr,  Deltag, T, Ui, mu, Delta_arr1, bd,  NDelta, skewed = False):
     # Here, Deltag must be the guess, if Delta < Deltag, 
-    # t, U, mu, mg, mz = param
 
     Delta = Delta_arr1.copy()
     for i in range(NDelta):
@@ -101,7 +100,7 @@ def calc_Delta_sc(Nx, Ny, m_arr, mz_arr, Delta_arr, Deltag,tol, Ui, mu,T,  Tc0, 
 
 # @njit()
 def calc_Tc_binomial(Nx, Ny, m_arr, mz_arr, Delta_arr, Deltag, Ui, mu, Tc0, bd, num_it, skewed, alignment = None):
-    N = 15 # Look at, maybe not needed this accuracy
+    N = 18 # Look at, maybe not needed this accuracy
     if alignment == None:
         assert bd[1] >= Nx
     # The first calculation is the same for all temperatures --------------
@@ -109,11 +108,11 @@ def calc_Tc_binomial(Nx, Ny, m_arr, mz_arr, Delta_arr, Deltag, Ui, mu, Tc0, bd, 
     # Delta_arr = (np.ones((Nx*Ny), dtype = complex)*Deltag).reshape(Ny, Nx)#*(x - bd)**2*0.05 / 40**2
     
     H = make_H_numba(Nx, Ny, m_arr, mz_arr, Delta_arr, mu, skewed)
-
     assert np.allclose(H, np.conjugate(H.T), rtol=1e-08, atol=1e-08, equal_nan=False)
 
     D, gamma = np.linalg.eigh(H)
     D, gamma = D[2*Nx * Ny:], gamma[:, 2*Nx * Ny:]
+
     # ---------------------------------------------------------------
 
     Ts_lower = 0
@@ -216,7 +215,12 @@ def make_system_normal(bd, U, mz, m, Deltag, Nx, Ny, alignment):
     # plt.imshow(m_arr)
     # plt.colorbar()
     # plt.show()
-
+    # plt.imshow(np.abs(Delta_arr))
+    # plt.colorbar()
+    # plt.show()    
+    # plt.imshow(np.abs(Ui))
+    # plt.colorbar()
+    # plt.show()
     if mz != 0:
 
         assert alignment == None
@@ -249,60 +253,84 @@ def make_system_one_material(bd, U, mz, m, Deltag, Nx, Ny):
 
 def plot_observables_constantT(Delta, gamma, D, T, Nx, Ny, NDelta, mz, bd, U, mu, Deltag, tol, alignment, skewed):
     x = np.arange(1, Nx + 1, 1)
-    ic(Delta, gamma, D, T, Nx, Ny, NDelta, mz, bd, U, mu, Deltag, tol, alignment, skewed)
+    ic( T, Nx, Ny, NDelta, mz, bd, U, mu, Deltag, tol, alignment, skewed)
     # -- Plot Delta as f.o. x --
     fig, ax = plt.subplots()
-    ax.plot(x, Delta[Ny//2, :])
+    ax.plot(x, np.abs(Delta[Ny//2, :]))
     # ax.axvline(x = bd[0])
-    ax.axvline(x = bd[1])
-    ax.set_ylabel(r"\Delta at y = " + str(Ny//2))
+    # ax.axvline(x = bd[1])
+    ax.set_ylabel(r"$\Delta$ at y = " + str(Ny//2))
     ax.set_xlabel(f"x")
     plt.show()
 
 
     # -- Plot the particle number as f.o. x --
-    N_up, N_dn = N_sigma(gamma, D, T, Nx, Ny)
+    N_up, N_dn = np.abs(N_sigma(gamma, D, T, Nx, Ny))
 
     fig, ax = plt.subplots()
-    ax.plot(x, N_up[Ny//2, :], label = r"N_{\uparrow}")
-    ax.plot(x, N_dn[Ny//2, :], label = r"N_{\downarrow}")
+    ax.plot(x, N_up[Ny//2, :], label = r"N_\uparrow")
+    ax.plot(x, N_dn[Ny//2, :], label = r"N_\downarrow")
 
-    ax.set_ylabel(r"Particle density at = " + str(Ny//2))
+    ax.set_ylabel(r"Particle density at y = " + str(Ny//2))
     ax.set_xlabel(f"x")
+
+    fig.legend()
+
+    print("Nup tot: ", np.sum(N_up))
+    print("Ndn tot: ", np.sum(N_dn))
+    print("Ntot: ", np.sum(N_dn) + np.sum(N_up))
     plt.show()
 
 
 def main(mg):
     t = 1.
-    Nx = 20
-    Ny = 20
-    NDelta = 10
+    Nx = 30
+    Ny = 10
+    NDelta = 5
     Tc0 = 0.2
     mz = 0.
-    bd = np.array([10,  Nx ])
+    bd = np.array([Nx//3,  (2 *Nx)//3 ])
+    ic(bd)
     U = 1.7
     mu = -0.5
     Deltag = 1e-5 + 0.j
     tol = 1e-6 
-    alignment = None
+    alignment = "AP"
     skewed = False
-    
-    # Ui, mz_arr, m_arr, Delta_arr = make_system_normal(bd, U, mz, mg, Deltag, Nx, Ny, alignment)
-    Ui, mz_arr, m_arr, Delta_arr = make_system_one_material(bd, U, mz, mg, Deltag, Nx, Ny)
+    Ui, mz_arr, m_arr, Delta_arr = make_system_normal(bd, U, mz, mg, Deltag, Nx, Ny, alignment)
+    # Ui, mz_arr, m_arr, Delta_arr = make_system_one_material(bd, U, mz, mg, Deltag, Nx, Ny)
 
     Tc = calc_Tc_binomial(Nx, Ny, m_arr, mz_arr, Delta_arr, Deltag, Ui, mu, Tc0, bd, NDelta, skewed, alignment)
     # T = Tc
     # Delta, gamma, D = calc_Delta_sc(Nx, Ny, m_arr, mz_arr, Delta_arr, Deltag, tol, Ui, mu,T,  Tc0, bd, NDelta, skewed, alignment)
 
     # plot_observables_constantT(Delta, gamma, D, T, Nx, Ny, NDelta, mz, bd, U, mu, Deltag, tol, alignment, skewed)
-    return Tc, NDelta, Ny
+    return Tc, NDelta, Ny, Nx, bd
 
 
 from multiprocess import Pool
+
 if __name__ == "__main__":
+    # args = []
+    # for mg in range(10, 20):
+    #     # for Ny in range(10, 20):
+    #     args.append((mg, Nx, Ny, mz, mu, ))
+    
+    # def fun(Nx, Ny):
+    #     return Nx + Ny
+
+    # def wrapper(args):
+    #     #Nx, Ny = args
+    #     #return (Nx, Ny, fun(Nx, Ny))
+    #     return (args, fun(*args))
+    
+    # print([*map(lambda args: (args, fun(*args)), args)])
+
+
     tic = time.time()
     # def f(x): return x*x
-    mgs = np.linspace(0, .1, 50)
+    # mgs = np.linspace(0, .1, 20)
+    mgs = np.linspace(0., 0.5, 50)
     # Nxs = np.arange(12, 32, 1)
     # NDs = np.arange(500, 520, 20)
     result = np.zeros_like(mgs)
@@ -310,7 +338,7 @@ if __name__ == "__main__":
     for i, mg in enumerate(mgs):
         ic(mg)
         if i ==0:
-            result[i], NDelta, Ny = main(mg)
+            result[i], NDelta, Ny, Nx, bd = main(mg)
         
         else:
             result[i] = main(mg)[0]
@@ -319,14 +347,13 @@ if __name__ == "__main__":
 
     ic(NDelta, Ny)
     # with Pool(len(mgs)) as p:
-       
 
-    #     result = p.map(main, mgs)
+    # starmap
 
     ic(result)
     # np.save("AParallell_mgdata", np.array([mgs, result]))
     # np.save("testdata/Nxs_straight", np.array([Nxs, result]))
-    np.save(f"NewData/OneMatND={NDelta}Ny={Ny}", np.array([mgs, NDelta, Ny, result], dtype=object))
+    np.save(f"NewData/APND={NDelta}Ny={Ny}Nx={Nx}bd={bd}", np.array([mgs, NDelta, Ny, result], dtype=object))
     # # ic(result.get())
     tac = time.time()
     ic(tac-tic)